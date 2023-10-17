6a271b0c8ade8dcc0135ade1c1086443f8542d6c
anvargasl <anvargas@udel.edu>
2023-10-17 15:14:51 -0400

    ading multithreading example, run 1 constantly updating OLED per core

--- OLED.py
+++ OLED.py
@@ -1,20 +1,33 @@
 from machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
 import framebuf, sys
 import utime
+import _thread
+
+pix_res_x = 128
+pix_res_y = 64
+
+font_height = 8
+margin = 5
+spacing = 2 #space between lines
+
+oled_dict = {0:{"scl":9, "sda":8}, 1:{"scl":27, "sda":26}}
 
 def init_i2c(Id, scl_pin, sda_pin):
     # Initialize I2C device
+    ident = str(_thread.get_ident())
+    
     i2c_dev = I2C(Id, scl=Pin(scl_pin), sda=Pin(sda_pin), freq=200000)
     i2c_addr = [hex(ii) for ii in i2c_dev.scan()]
     
     if not i2c_addr:
-        print('No I2C Display Found')
+        print(ident+' No I2C Display Found')
         sys.exit()
     else:
-        print("I2C Address      : {}".format(i2c_addr))
-        print("I2C Configuration: {}".format(i2c_dev))
+        print(ident+" I2C Address      : {}".format(i2c_addr)+"\n")
+        print(ident+" I2C Configuration: {}".format(i2c_dev)+"\n")
     
-    return i2c_dev
+    return i2c_dev, ident
 
 def display_logo(oled):
     # Display the Raspberry Pi logo on the OLED
@@ -23,32 +36,72 @@ def display_logo(oled):
     oled.fill(0)
     oled.blit(fb, 96, 0)
     oled.show()
-    
-def display_text(oled, text="Raspberry Pi"):
+
+def display_text(oled, line, text="Raspberry Pi"):
     # Display text on the OLED
-    oled.text(text, 5, 5)
+    oled.text(text, margin, margin+line*(font_height+spacing))
     #oled.text("Pico", 5, 15)
     oled.show()
     
-def display_anima(oled):
+def display_timer(oled):
     # Display a simple timer animation on the OLED
     start_time = utime.ticks_ms()
 
     while True:
         elapsed_time = (utime.ticks_diff(utime.ticks_ms(), start_time) // 1000) + 1
         
-        text_x = 5
-        text_y = oled.height-8-5 #8=font height, 5=margin
+        text_y = oled.height-font_height-margin
         
         # Clear the specific line by drawing a filled black rectangle
-        width = oled.width-text_x-2
-        oled.fill_rect(text_x, text_y, width, 8, 0)
+        width = oled.width-2*margin
+        oled.fill_rect(margin, text_y, width, font_height, 0)
 
-        oled.text("Timer:", 5, 30)
-        oled.text(str(elapsed_time) + " sec", text_x, text_y)
+        oled.text("timer:", margin, text_y-font_height-spacing)
+        oled.text(str(elapsed_time) + " sec", margin, text_y)
         oled.show()
         utime.sleep_ms(1000)
         
+def display_counter(oled):
+    # Display a simple timer animation on the OLED
+    counter = 0
+    while True:
+        text_y = oled.height-font_height-margin
+        
+        # Clear the specific line by drawing a filled black rectangle
+        width = oled.width-2*margin
+        oled.fill_rect(margin, text_y, width, font_height, 0)
+
+        oled.text("counter:", margin, text_y-font_height-spacing)
+        oled.text(str(counter), margin, text_y)
+        oled.show()
+        utime.sleep_ms(2000)
+        
+        counter += 2
+
+#display a timer or a even counter
+def display_task(Id, task):
+    '''if Id != 0 or Id != 1:
+        print("valid Ids are 0 or 1")
+        quit()
+    if task != "timer" or task != "counter"
+        print("valid tasks are \"timer\" or \"counter\"")
+        quit()
+    '''
+    #global oled_dict
+    i2c_dev, ident = init_i2c(Id=Id, scl_pin=oled_dict[Id]["scl"], sda_pin=oled_dict[Id]["sda"])
+    oled = SSD1306_I2C(pix_res_x, pix_res_y, i2c_dev)
+    
+    display_logo(oled)
+    display_text(oled, line=0, text="Thread")
+    display_text(oled, line=1, text=ident)
+    oled.rect(0,0,pix_res_x,pix_res_y,1)
+    oled.show()
+    
+    if task=="timer":
+        display_timer(oled)
+    elif task=="counter":
+        display_counter(oled)
+
 #MicroPython logo
 '''
 oled.fill(0)
--- main.py
+++ main.py
@@ -1,23 +1,11 @@
-from ssd1306 import SSD1306_I2C
 from utime import sleep
 import OLED
 import _thread
-
-pix_res_x = 128
-pix_res_y = 64
-
+    
 def main():
-    i2c_dev0 = OLED.init_i2c(Id=0, scl_pin=9, sda_pin=8)
-    i2c_dev1 = OLED.init_i2c(Id=1, scl_pin=27, sda_pin=26)
-    oled0 = SSD1306_I2C(pix_res_x, pix_res_y, i2c_dev0)
-    oled1 = SSD1306_I2C(pix_res_x, pix_res_y, i2c_dev1)
     
-    OLED.display_logo(oled1)
-    oled1.rect(0,0,128,64,1)
-    oled1.show()
+    new_thread = _thread.start_new_thread(OLED.display_task, (1, "timer"))
+    OLED.display_task(Id=0, task="timer")
     
-    OLED.display_text(oled0, text="\n Magnolia")
-    OLED.display_anima(oled0)
-
 if __name__ == '__main__':
     main()
