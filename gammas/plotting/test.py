c597bd32b4188cc2fd5424886801d6423e15e8da
anvargasl <anvargas@udel.edu>
2024-05-31 19:46:35 -0500

    adding 1h background data taken with the Pico

--- gammas/plotting/test.py
+++ gammas/plotting/test.py
@@ -1,32 +1,7 @@
-import json 
+import pandas as pd
 
-with open("../data/Ba133-october-12-2023/calibration.json", "r") as infile:
-    calibration = json.load(infile)
+df = pd.read_csv("../data/PICO/bkgd.txt", delimiter="\t", skiprows=1, names=["event", "adc", "deadt"])
 
-# Data to be written 
-'''dictionary = {
-    "prefixes" : ["Ba133"],
-    "prefix_bkgd" : "bkgd",
-    "params_dict" : {"Ba133": [[100, 0, 1000, 2, 1]]},
-    "range_dict" : {"Ba133": [[0, 2.5]]},
-    "peak_dict" : {"Ba133": ["31 keV"]},
-    "gauss_centers" : {"Ba133": []},
-	"gauss_centers_err" : {"Ba133": []}
-}'''
+duplicates = df["event"].duplicated()
+print(df.loc[duplicates])
 
-dictionary = {
-    "prefixes" : ["Ba133"],
-    "prefix_bkgd" : "bkgd",
-    "peak_dict" : {"Ba133": {	"31 keV": {"range": [0, 3], "fit_init": [0, 0, 1, 1.87, 1], "fit_results": None},
-    							"81 keV": {"range": [3, 7], "fit_init": [0.1, -1, 0.18, 6, 1], "fit_results": None}}},
-}
-
-with open("../data/Ba133-october-12-2023/calibration.json", "w") as outfile: 
-    json.dump(dictionary, outfile, indent=4)
-
-with open("test.json", "r") as infile: 
-    calibration = json.load(infile)
-
-prefix = "Ba133"
-peak = "31 keV"
-print(calibration["peak_dict"][prefix][peak]["fit_init"])
